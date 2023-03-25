7a0c97b2df6ce1030212b12e8a6e009cc7eb063d
Paul Oh <paul.oh1221@gmail.com>
2023-03-25 01:26:56 -0400

    removed unused folders

--- src/api/query.js
+++
@@ -1,9 +0,0 @@
-// import axios from "axios";
-
-// const url = "http://localhost:8080/api"
-
-// export const getCountries = async (name) => {
-//     return await axios.get(url + `/airline-countries/${name}`).then((res) =>{
-//         console.log(res);
-//     });
-// }
